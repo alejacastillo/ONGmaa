4316fc7fa879363caa447ed779ce7839f0034789
DELL <DELL@DESKTOP-PV5V4EA>
2021-02-17 17:26:57 -0600

    Agregando Conexion

---
+++ ONG/ONG.BL/DesaparecidosBL.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ONG.BL
+{
+    public class DesaparecidosBL
+    {
+            Contexto _contexto;
+
+        public List<Desaparecidos> listaDesaparecidos { get; set; }
+
+    public DesaparecidosBL()
+        {
+            _contexto = new Contexto();
+            listaDesaparecidos = new List<Desaparecidos>();
+        }
+      
+        public List<Desaparecidos> ObtenerDesaparecidos()
+        {
+            listaDesaparecidos = _contexto.Desaparecido.ToList();
+          
+
+            return listaDesaparecidos;
+          }
+    }
+}
